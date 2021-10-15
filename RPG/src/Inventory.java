578420a8388a510776f6ee5191d619e69fb90a2a
Kim-Hyong-Min <92084553+Kim-Hyong-Min@users.noreply.github.com>
2021-10-15 16:45:24 +0900

    Update Inventory.java

--- RPG/src/Inventory.java
+++ RPG/src/Inventory.java
@@ -1,4 +1,14 @@
 
 public class Inventory {
   private int itemCode
+  private int playerCode;
+  
+  public void itemList(){ //보유 아이템
+    
+  }
+  
+  public void playerItem(){//유저 장비 장착/해제
+    
+  }
+    
 }
