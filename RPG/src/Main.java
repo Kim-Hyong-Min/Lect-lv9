fb989767ab04015b53c10e7d2c8ade619a61b118
Kim-Hyong-Min <92084553+Kim-Hyong-Min@users.noreply.github.com>
2021-10-15 21:00:25 +0900

    Update Main.java

--- RPG/src/Main.java
+++ RPG/src/Main.java
@@ -1,11 +1,15 @@
+import java.util.Random;
 
 public class Main {
-	private Shop sp = new Shop();
+	public static Random rn = new Random();
+	public static Shop sp = new Shop();
 	public static Guild gd = new Guild();
+	private Unit ut = Unit.instance;
 	
 // 메인메뉴
 // ㄴ 길드관리, 상점, 인벤토리, 저장, 로드
 	public void run() {
+		ut.playerSet();
 		gd.guildSet();
 		sp.weaponSet();
 		sp.armorSet();
@@ -26,7 +30,7 @@ public class Main {
 		try {
 			int num = Integer.parseInt(input);
 			if(num==1) {//길드관리
-				
+				gd.guildMenu();
 			}
 			else if(num==2) {//상점
 				sp.shopMenu();
