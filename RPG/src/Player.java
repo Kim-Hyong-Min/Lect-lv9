7b0977ecf7855c8f596c7fb5a66c93d68b383f7b
Kim-Hyong-Min <92084553+Kim-Hyong-Min@users.noreply.github.com>
2021-10-15 21:02:08 +0900

    Update Player.java

--- RPG/src/Player.java
+++ RPG/src/Player.java
@@ -12,4 +12,52 @@ public class Player {
 	private int playerCode;
 	private int[] playerItem = new int[3]; // 0:무기 / 1:갑옷 / 2:반지
 	// 사망시 장비 인벤토리로 귀속
+	
+	Player() {	
+	}
+	
+	public Player(String name, int maxHp, int atk, int def, int playerCode){//첫 생성
+		this.name = name;
+		this.level = 1;
+		this.maxHp = maxHp;
+		this.hp = this.maxHp;
+		this.atk = atk;
+		this.def = def;
+		this.exp = 0;
+		this.playerCode = playerCode;
+	}
+	
+	public int getPlayerCode() {
+		return this.playerCode;
+	}
+	
+	public int getPlayerItem(int idx) {
+		return this.playerItem[idx];
+	}
+	
+	
+	public void printPlayer() {
+		System.out.printf("[%s] [Lv.%d] [HP:%d/%d] [ATK:%d] [DEF:%d] [EXP:%d] ",this.name, this.level, this.hp, this.maxHp, this.atk, this.def, this.exp);
+		for(int i=0; i<this.playerItem.length; i++) {
+			if(i==0) {
+				System.out.print("[무기:");
+			}
+			else if(i==1) {
+				System.out.print("[갑옷:");
+			}
+			else if(i==2) {
+				System.out.print("[반지:");
+			}
+			if(this.playerItem[i]!=0) {
+				for(int j=0; j<Main.sp.item.size(); j++) {
+					if(this.playerItem[i]==Main.sp.item.get(j).getItemCode()) {
+						System.out.printf("%s] ",Main.sp.item.get(j).getName());
+					}
+				}
+			}
+			else System.out.print("없음] ");
+		}
+		System.out.println();
+	}
+	
 }
