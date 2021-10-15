6f3685db99faeafd6d952dff912d7d630ea1cfd5
Kim-Hyong-Min <92084553+Kim-Hyong-Min@users.noreply.github.com>
2021-10-15 18:14:49 +0900

    Update Item.java

--- RPG/src/Item.java
+++ RPG/src/Item.java
@@ -5,5 +5,32 @@ public class Item {
 	private int def;
 	private int price;
 	private int itemCode;
-
+	
+	public Item(String name, int atk, int def, int price, int itemCode){
+		this.name = name;
+		this.atk = atk;
+		this.def = def;
+		this.price = price;
+		this.itemCode = itemCode;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getAtk() {
+		return this.atk;
+	}
+	
+	public int getDef() {
+		return this.def;
+	}
+	
+	public int getPrice() {
+		return this.price;
+	}
+	
+	public int getItemCode() {
+		return this.itemCode;
+	}
 }
