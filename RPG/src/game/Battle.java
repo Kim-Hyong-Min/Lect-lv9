dbf5fbfcd41e965eae0d710880ea874d25f07abe
lk823 <lk823@DESKTOP-UHUQ99P>
2021-10-21 21:06:38 +0900

    start

--- RPG/src/game/Battle.java
+++ RPG/src/game/Battle.java
@@ -7,9 +7,9 @@ public class Battle {
 	public static ArrayList<Monster> Monster = new ArrayList<>();
 	
 	private void monsterSet() {
-		Monster py = new Monster("고블린", 1, 15, 1, 1, 10);
+		Monster py = new Monster("요정", 1, 15, 1, 1, 10);
 		Monster.add(py);
-		py = new Monster("오우거", 2, 20, 2, 2, 10);
+		py = new Monster("고블린", 2, 20, 2, 2, 10);
 		Monster.add(py);
 		py = new Monster("늑대", 3, 25, 3, 3, 10);
 		Monster.add(py);
@@ -17,7 +17,7 @@ public class Battle {
 		Monster.add(py);
 		py = new Monster("슬라임", 5, 35, 5, 6, 10);
 		Monster.add(py);
-		py = new Monster("코끼리", 6, 40, 6, 5, 10);
+		py = new Monster("오우거", 6, 40, 6, 5, 10);
 		Monster.add(py);
 		py = new Monster("유령", 7, 45, 7, 7, 10);
 		Monster.add(py);
@@ -32,30 +32,15 @@ public class Battle {
 	// 사냥터 : 1,2/3,4/5,6/7,8,9/10
 	
 	public void battleMap() {
-		System.out.println("1.초보자 사냥터(Lv.1~2)\n2.숲 사냥터(Lv.3~4)\n3.초보자 사냥터(Lv.5~6)\n4.지옥 사냥터\n5.마왕성(Lv.10)\n6.뒤로가기");
+		System.out.println("1.초보자 사냥터(Lv.1~2)\n2.숲(Lv.3~4)\n3.황야(Lv.5~6)\n4.지옥(Lv.7~9)\n5.마왕성(Lv.10)\n6.뒤로가기");
 		while(true) {
 		String input = Shop.sc.next();
 		try {
 			int num = Integer.parseInt(input);
-			if(num==1) {//
+			if(num>=1 && num<=5) {// 초보자
 				
 			}
-			else if(num==2) {//
-				
-			}
-			else if(num==3) {//
-				
-			}
-			else if(num==4) {
-				
-			}
-			else if(num==5) {//
-				
-			}
-			else if(num==6) {//
-				
-			}
-			else if(num==7) {//
+			else if(num==6) {// 뒤로가기
 				
 			}
 			
@@ -63,4 +48,31 @@ public class Battle {
 		}
 	}
 	}
+	
+	public void battleStart(int num) { // 0,1 / 2,3 / 4,5 / 6,7,8 / 9
+		if(num==1) {// 초보자
+			
+		}
+		else if(num==2) {// 숲
+			
+		}
+		else if(num==3) {// 황야
+			
+		}
+		else if(num==4) {// 지옥
+			
+		}
+		else if(num==5) {// 마왕성
+			
+		}
+		int rNum = Main.rn.nextInt();
+	}
+	
+	public void battleUi() {
+		
+	}
+	
+	
+	
+	
 }
--- RPG/src/game/Monster.java
+++ RPG/src/game/Monster.java
@@ -10,12 +10,18 @@ public class Monster {
 	private int exp;
 	private int Item;
 	
-	Monster (String name, int level, int maxHp, int atk, int def, int exp){
+	public Monster (String name, int level, int maxHp, int atk, int def, int exp){
 		this.name = name;
 		this.level = level;
 		this.maxHp = maxHp;
 		this.atk = atk;
 		this.def = def;
 		this.exp = exp;
+	}
+	
+	
+	public void printMonster() {
+		System.out.printf("	¿Ã∏ß : %s\n",this.name);
+		System.out.printf("	Lv.%d | HP %d/%d \n",this.level, this.hp, this.maxHp);
 	}
 }
