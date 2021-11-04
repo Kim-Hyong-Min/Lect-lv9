8d7d9651b3f142a1135e1f86b2c88d82f3c52f10
lk823 <lk823@DESKTOP-UHUQ99P>
2021-11-04 17:49:42 +0900

    endd

--- gui/src/basic/문제02.java
+++ gui/src/basic/문제02.java
@@ -14,12 +14,13 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-class GameWin extends JFrame{
+class GameWin extends JFrame {
 	private JLabel jb = new JLabel();
+
 	public GameWin() {
 		super("Clear!!");
 		setLayout(null);
-		setBounds(0,0, 300, 200);
+		setBounds(0, 0, 300, 200);
 		jb.setBounds(35, 0, 200, 150);
 		jb.setFont(new Font("", Font.PLAIN, 20));
 		setTime();
@@ -29,88 +30,83 @@ class GameWin extends JFrame{
 		revalidate();
 		MyJFrame.reStart();
 	}
-	
+
 	private void setTime() {
-		if(MyJFrame.min==0) {
-			if(MyJFrame.sec<10) {
-				jb.setText("클리어 시간 : 00:0"+MyJFrame.sec);
+		if (MyJFrame.min == 0) {
+			if (MyJFrame.sec < 10) {
+				jb.setText("클리어 시간 : 00:0" + MyJFrame.sec);
+			} else {
+				jb.setText("클리어 시간 : 00:" + MyJFrame.sec);
 			}
-			else {
-				jb.setText("클리어 시간 : 00:"+MyJFrame.sec);
-			}
-		}
-		else {
-			if(MyJFrame.min<10) {
-				if(MyJFrame.sec<10) {
-					jb.setText("클리어 시간 : 0"+MyJFrame.min+":0"+MyJFrame.sec);
+		} else {
+			if (MyJFrame.min < 10) {
+				if (MyJFrame.sec < 10) {
+					jb.setText("클리어 시간 : 0" + MyJFrame.min + ":0" + MyJFrame.sec);
+				} else {
+					jb.setText("클리어 시간 : 0" + MyJFrame.min + ":" + MyJFrame.sec);
 				}
-				else {
-					jb.setText("클리어 시간 : 0"+MyJFrame.min+":"+MyJFrame.sec);
-				}
-			}
-			else {
-				if(MyJFrame.sec<10) {
-					jb.setText("클리어 시간 : "+MyJFrame.min+":0"+MyJFrame.sec);
-				}
-				else {
-					jb.setText("클리어 시간 : "+MyJFrame.min+":"+MyJFrame.sec);
+			} else {
+				if (MyJFrame.sec < 10) {
+					jb.setText("클리어 시간 : " + MyJFrame.min + ":0" + MyJFrame.sec);
+				} else {
+					jb.setText("클리어 시간 : " + MyJFrame.min + ":" + MyJFrame.sec);
 				}
 			}
 		}
 	}
-	
+
 }
 
-class Game extends JPanel implements ActionListener{
+class Game extends JPanel implements ActionListener {
 	public static Random rn = new Random();
 	public static final int NUM = 25;
-	public static JButton number[] = new JButton [NUM];
-	public static int front[] = new int [NUM];
-	public static int back[] = new int [NUM];
-	public static int check[] = new int [NUM];
+	public static JButton number[] = new JButton[NUM];
+	public static int front[] = new int[NUM];
+	public static int back[] = new int[NUM];
+	public static int check[] = new int[NUM];
 	public static int cnt = 1;
-	
+
 	public Game() {
 		setGame();
 		setLayout(null);
-		setBounds(0,0,MyJFrame.total,MyJFrame.total);
+		setBounds(0, 0, MyJFrame.total, MyJFrame.total);
 		setButton();
 	}
-	
+
 	private void win() {
-		if(cnt>this.NUM*2) {
+		if (cnt > this.NUM * 2) {
 			MyJFrame.finish = true;
 			GameWin gw = new GameWin();
 		}
 	}
-	
+
 	public static void setGame() {
-		for(int i=0; i<NUM; i++) {
-			front[i]= i+1;
-			back[i] = i+1+NUM;
+		for (int i = 0; i < NUM; i++) {
+			front[i] = i + 1;
+			back[i] = i + 1 + NUM;
 		}
-		
+
 		// 셔플
-		for(int i=0; i<1000; i++) {
+		for (int i = 0; i < 1000; i++) {
 			int rNum = rn.nextInt(NUM);
 			int temp = front[0];
 			front[0] = front[rNum];
 			front[rNum] = temp;
-			
+
 			rNum = rn.nextInt(NUM);
 			temp = back[0];
 			back[0] = back[rNum];
-			back[rNum]=temp;
+			back[rNum] = temp;
 		}
 	}
-	
+
 	private void setButton() {// 400-250 = 150
-		int w = MyJFrame.total/2-100*5/2;
-		int h = MyJFrame.total/2-100*5/2;
-		for(int i=0; i<this.NUM; i++) {//0,100,200,300,400 / 5,10,15,20
-			if(i != 0 && i%5==0) {
+		int w = MyJFrame.total / 2 - 100 * 5 / 2;
+		int h = MyJFrame.total / 2 - 100 * 5 / 2;
+		for (int i = 0; i < this.NUM; i++) {// 0,100,200,300,400 / 5,10,15,20
+			if (i != 0 && i % 5 == 0) {
 				h += 100;
-				w = MyJFrame.total/2-100*5/2;
+				w = MyJFrame.total / 2 - 100 * 5 / 2;
 			}
 			this.number[i] = new JButton();
 			this.number[i].setBounds(w, h, 100, 100);
@@ -118,27 +114,24 @@ class Game extends JPanel implements ActionListener{
 			this.number[i].addActionListener(this);
 			this.number[i].setFont(new Font("", Font.BOLD, 20));
 			this.number[i].setText(String.valueOf(this.front[i]));
-			
+
 			add(this.number[i]);
 			w += 100;
 		}
 	}
-	
-	
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+
 		JButton push = (JButton) e.getSource();
-		
-		for(int i=0; i<this.NUM; i++) {
-			if(push == this.number[i] && this.number[i].getText().equals(String.valueOf(cnt))) {
-				if(cnt<=this.NUM) {//1~25
+
+		for (int i = 0; i < this.NUM; i++) {
+			if (push == this.number[i] && this.number[i].getText().equals(String.valueOf(cnt))) {
+				if (cnt <= this.NUM) {// 1~25
 					this.number[i].setBackground(new Color(255, 130, 67));
 					this.number[i].setFont(new Font("", Font.BOLD, 20));
 					this.number[i].setText(String.valueOf(this.back[i]));
-				}
-				else {//26~50
+				} else {// 26~50
 					this.number[i].setBackground(new Color(255, 208, 127));
 					this.number[i].setFont(new Font("", Font.BOLD, 20));
 					this.number[i].setText("");
@@ -147,28 +140,27 @@ class Game extends JPanel implements ActionListener{
 				win();
 			}
 		}
-		
+
 	}
-	
+
 }
 
-class MyJFrame extends JFrame implements ActionListener, Runnable{
+class MyJFrame extends JFrame implements ActionListener, Runnable {
 	public static Dimension dm = Toolkit.getDefaultToolkit().getScreenSize();
 	public static JLabel jb = new JLabel();
 	public static int w = dm.width;
 	public static int h = dm.height;
 	public static final int total = 800;
 	private static JButton re = new JButton();
-	
 
 	public static int min = 0;
 	public static int sec = 0;
 	public static boolean finish;
-	
+
 	public MyJFrame() {
 		super("1 to 50");
 		setLayout(null);
-		setBounds(w/2-total/2,h/2-total/2,total,total);
+		setBounds(w / 2 - total / 2, h / 2 - total / 2, total, total);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		jb.setBounds(300, 0, 200, 150);
 		jb.setFont(new Font("", Font.BOLD, 30));
@@ -180,29 +172,30 @@ class MyJFrame extends JFrame implements ActionListener, Runnable{
 		revalidate();
 		setReset();
 	}
-	
+
 	public static void reStart() {
-		Game.front = new int [Game.NUM];
-		Game.back = new int [Game.NUM];
-		Game.check = new int [Game.NUM];
+		Game.front = new int[Game.NUM];
+		Game.back = new int[Game.NUM];
+		Game.check = new int[Game.NUM];
 		Game.cnt = 1;
 		min = 0;
 		sec = 0;
 		finish = true;
 		jb.setText("ready");
 		re.setText("START");
+		re.setBackground(new Color(47, 221, 146));
 		reset();
 	}
-	
+
 	private static void reset() {
 		Game.setGame();
-		for(int i=0; i<Game.NUM; i++) {
+		for (int i = 0; i < Game.NUM; i++) {
 			Game.number[i].setBackground(new Color(255, 208, 127));
 			Game.number[i].setFont(new Font("", Font.BOLD, 20));
 			Game.number[i].setText(String.valueOf(Game.front[i]));
 		}
 	}
-	
+
 	private void setReset() {
 		re.setBounds(350, 700, 100, 50);
 		re.setBackground(new Color(47, 134, 166));
@@ -211,44 +204,37 @@ class MyJFrame extends JFrame implements ActionListener, Runnable{
 		re.addActionListener(this);
 		add(re);
 	}
-	
-	
-	
+
 	@Override
 	public void run() {
 		System.out.println("start");
-		while(!finish) {
-			if(this.min==0) {
-				if(this.sec<10) {
-					jb.setText("00:0"+this.sec);
-					
+		while (!finish) {
+			if (this.min == 0) {
+				if (this.sec < 10) {
+					jb.setText("00:0" + this.sec);
+
+				} else {
+					jb.setText("00:" + this.sec);
 				}
-				else {
-					jb.setText("00:"+this.sec);
-				}
-			}
-			else {
-				if(this.min<10) {
-					if(this.sec<10) {
-						jb.setText("0"+this.min+":0"+this.sec);
-						
+			} else {
+				if (this.min < 10) {
+					if (this.sec < 10) {
+						jb.setText("0" + this.min + ":0" + this.sec);
+
+					} else {
+						jb.setText("0" + this.min + ":" + this.sec);
 					}
-					else {
-						jb.setText("0"+this.min+":"+this.sec);
-					}
-				}
-				else {
-					if(this.sec<10) {
-						jb.setText(this.min+":0"+this.sec);
-						
-					}
-					else {
-						jb.setText(this.min+":"+this.sec);
+				} else {
+					if (this.sec < 10) {
+						jb.setText(this.min + ":0" + this.sec);
+
+					} else {
+						jb.setText(this.min + ":" + this.sec);
 					}
 				}
 			}
 			this.sec++;
-			if(this.sec==60) {
+			if (this.sec == 60) {
 				this.min++;
 				this.sec = 0;
 			}
@@ -257,15 +243,15 @@ class MyJFrame extends JFrame implements ActionListener, Runnable{
 			} catch (Exception e) {
 			}
 		}
-		
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(this.re == e.getSource() && finish == false) { // 시간 리셋, front/back/check 리셋, cnt 리셋, 버튼 세팅 리셋
-			Game.front = new int [Game.NUM];
-			Game.back = new int [Game.NUM];
-			Game.check = new int [Game.NUM];
+		if (this.re == e.getSource() && finish == false) { // 시간 리셋, front/back/check 리셋, cnt 리셋, 버튼 세팅 리셋
+			Game.front = new int[Game.NUM];
+			Game.back = new int[Game.NUM];
+			Game.check = new int[Game.NUM];
 			Game.cnt = 1;
 			min = 0;
 			sec = 0;
@@ -274,27 +260,27 @@ class MyJFrame extends JFrame implements ActionListener, Runnable{
 			re.setText("START");
 			re.setBackground(new Color(47, 221, 146));
 			reset();
-		}
-		else if(this.re == e.getSource() && finish == true) {
+		} else if (this.re == e.getSource() && finish == true) {
 			finish = false;
 			re.setText("RESET");
 			re.setBackground(new Color(47, 134, 166));
 			문제02.run();
 		}
-		
+
 	}
 
 }
+
 public class 문제02 {
 	public static Runnable mf = new MyJFrame();
+
 	public static void run() {
 		Thread thread = new Thread(mf);
-			thread.start();
+		thread.start();
 	}
-	
+
 	public static void main(String[] args) {
 		run();
 
-		
 	}
 }
